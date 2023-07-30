230bd01b4207a4bdd8ed93440b2c27208fbafe7f
Elliot Potter <elliotpotter496@gmail.com>
2023-07-29 17:46:08 -0700

    try / catch-ify existing logic

--- routes/room/formRoom.js
+++ routes/room/formRoom.js
@@ -3,108 +3,96 @@ const RoomModel = require("../../models/RoomModel");
 const {roomSelectionCriteria} = require("./roomSelectionCriteria");
 
 const formRoomFunction = (cognitoId) => {
-    return new Promise((resolve, reject) => {
-        UserModel.findOne({cognitoId})
-            .exec()
-            .catch(err => {
-                console.error(err)
-                return reject("Error occurred while finding user with this ID")
-            })
-            .then(async(user) => {
-                if (!user) {
-                    return reject("Unable to find a user with this ID")
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = UserModel.findOne({cognitoId}).exec()
+            if (!user) {
+                return reject("Unable to find a user with this ID")
+            }
+
+            else {
+                console.log(user)
+
+                // to simplify things: randomly choose one of the groups this person is interested in
+                let choice = user.lookingFor[Math.floor(Math.random() * user.lookingFor.length)]
+
+                let isOneSided = false
+                if (choice + "" === user.identity + "") {
+                    isOneSided = true
                 }
 
+                console.log((user.maxDistance / 2) * 1609)
+
+                let [dates, competitors] = await Promise.all([
+                    // find dates near these coordinates
+                    // identity and choice are flipped, because we want to find someone who is LOOKING FOR
+                    // the user
+                    UserModel.find(roomSelectionCriteria(user, user.identity, choice))
+                        .limit(10)
+                        .select(["_id", "waitingForRoom", "location", "identity"])
+                        .exec(),
+
+                    // if it's one-sided, competitors=dates
+                    isOneSided ? new Promise((resolve, reject) => resolve([])) :
+
+                        // find competitors near these coordinates
+                        UserModel.find(roomSelectionCriteria(user, choice, user.identity))
+                            .limit(9)
+                            .select(["_id", "waitingForRoom", "location", "identity"])
+                            .exec(),
+                ])
+
+                console.log(`completed find; ${dates.length} dates and ${competitors.length} competitors`)
+
+                if (isOneSided) {
+                    if (dates.length < 10) {
+                        console.log("rejecting... 1")
+                        return reject("Not enough dates for one-sided dating")
+                    }
+                    else {
+                        dates.push(user)
+                    }
+                }
                 else {
-                    console.log(user)
-
-                    // to simplify things: randomly choose one of the groups this person is interested in
-                    let choice = user.lookingFor[Math.floor(Math.random() * user.lookingFor.length)]
-
-                    let isOneSided = false
-                    if (choice + "" === user.identity + "") {
-                        isOneSided = true
+                    if (dates.length < 10 || competitors.length < 9) {
+                        console.log("rejecting... 2")
+                        return reject("Too few dates or competitors")
                     }
-
-                    console.log((user.maxDistance / 2) * 1609)
-
-                    try {
-                        let [dates, competitors] = await Promise.all([
-                            // find dates near these coordinates
-                            // identity and choice are flipped, because we want to find someone who is LOOKING FOR
-                            // the user
-                            UserModel.find(roomSelectionCriteria(user, user.identity, choice))
-                                .limit(10)
-                                .select(["_id", "waitingForRoom", "location", "identity"])
-                                .exec(),
-
-                            // if it's one-sided, competitors=dates
-                            isOneSided ? new Promise((resolve, reject) => resolve([])) :
-
-                            // find competitors near these coordinates
-                            UserModel.find(roomSelectionCriteria(user, choice, user.identity))
-                                .limit(9)
-                                .select(["_id", "waitingForRoom", "location", "identity"])
-                                .exec(),
-                        ])
-
-                        console.log(`completed find; ${dates.length} dates and ${competitors.length} competitors`)
-
-                        if (isOneSided) {
-                            if (dates.length < 10) {
-                                console.log("rejecting... 1")
-                                return reject("Not enough dates for one-sided dating")
-                            }
-                            else {
-                                dates.push(user)
-                            }
-                        }
-                        else {
-                            if (dates.length < 10 || competitors.length < 9) {
-                                console.log("rejecting... 2")
-                                return reject("Too few dates or competitors")
-                            }
-                            else {
-                                competitors.push(user)
-                            }
-                        }
-
-                        console.log("finished manipulating arrays")
-
-                        let room = new RoomModel({
-                            spawningUser: user,
-                            numPeople: dates.length + competitors.length,
-                            isSingleSided: isOneSided,
-                            sideOne: dates,
-                            sideOneIdentity: choice,
-                            sideTwo: competitors,
-                            sideTwoIdentity: identity,
-                        })
-
-                        console.log("Saving room")
-                        // save this new room
-                        room.save().then(() => {
-                            // TODO -- change both lists to mark them as no longer waiting for a room
-                            // wait until I've debugged this, though
-
-                            console.log({dates, competitors})
-
-                            return resolve({
-                                dates, competitors
-                            })
-                        })
-                        .catch((err) => {
-                            console.error(err)
-                            return reject("Error saving a new room")
-                        })
+                    else {
+                        competitors.push(user)
                     }
-                    catch (err) {
-                        console.error(err)
-                        return reject(err)
-                    }
-
                 }
-            })
+
+                console.log("finished manipulating arrays")
+
+                let room = new RoomModel({
+                    spawningUser: user,
+                    numPeople: dates.length + competitors.length,
+                    isSingleSided: isOneSided,
+                    sideOne: dates,
+                    sideOneIdentity: choice,
+                    sideTwo: competitors,
+                    sideTwoIdentity: identity,
+                })
+
+                console.log("Saving room")
+                // save this new room
+                await room.save()
+
+                // TODO -- change both lists to mark them as no longer waiting for a room
+                // wait until I've debugged this, though
+
+                console.log({dates, competitors})
+
+                return resolve({
+                    dates, competitors
+                })
+            }
+        }
+        catch (err) {
+            console.error(err)
+            return reject(err)
+        }
     })
 }
 
--- routes/room/replaceUserInRoom.js
+++ routes/room/replaceUserInRoom.js
@@ -2,12 +2,118 @@
 // (this SHOULD be only one room)
 // then remove them from the room and swap in a suitable alternative
 const RoomModel = require("../../models/RoomModel");
+const {roomSelectionCriteria} = require("./roomSelectionCriteria");
+const UserModel = require("../../models/UserModel");
 
-async function replaceUserInRoom(userObject) {
-    await Promise.all([
+async function findRoomsWithUsers(userObject) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let [onSideOne, onSideTwo] = await Promise.all([
+                // find any room that has this user on SideOne
+                RoomModel.findOne({sideOne: userObject._id}).populate(["spawningUser"]).exec(),
+                // find any room that has this user on SideTwo
+                RoomModel.findOne({sideTwo: userObject._id}).populate(["spawningUser"]).exec(),
+            ])
 
-    ])
-
+            if (onSideTwo) {
+                resolve ({room: onSideTwo, onSideTwo: true})
+            }
+            else resolve ({room: onSideOne, onSideTwo: false})
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
 }
 
-module.exports = {replaceUserInRoom}
+/*
+Removes the user from a room and DOES NOT replace them
+(this occurs when the user completes a review after a date, or after a setup)
+ */
+async function removeUserFromRoom(userObject) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let {room, onSideTwo} = await findRoomsWithUsers(userObject)
+            // depending on what side we're on, find the user and remove them from that side
+            let workingArray = []
+            if (onSideTwo) {
+                for (let userIndex = 0; userIndex < room.sideTwo.length; userIndex++) {
+                    if (room.sideTwo[userIndex] + "" !== userObject._id + "") {
+                        workingArray.push(room.sideTwo[userIndex])
+                    }
+                }
+                room.sideTwo = workingArray
+            }
+            else {
+                for (let userIndex = 0; userIndex < room.sideOne.length; userIndex++) {
+                    if (room.sideOne[userIndex] + "" !== userObject._id + "") {
+                        workingArray.push(room.sideOne[userIndex])
+                    }
+                }
+                room.sideOne = workingArray
+            }
+
+            await room.save().exec()
+            resolve("Removing user from room successful")
+        }
+        catch(err) {
+            reject(err)
+        }
+    })
+}
+
+/*
+Removes the user from a room and DOES replace them
+(this occurs when the user opts to swap themselves out, due to distance, inactivity or lack of interest)
+ */
+async function replaceUserInRoom(userObject) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let {room, onSideTwo} = await findRoomsWithUsers(userObject)
+
+            // set what the person is looking for and what their identity is
+            let targetIdentity = onSideTwo ? room.sideTwoIdentity : sideOneIdentity
+            let targetLookingFor = onSideTwo ? room.sideOneIdentity : sideTwoIdentity
+
+            // depending on what side we're on, find the user and remove them from that side
+            let workingArray = []
+            if (onSideTwo) {
+                for (let userIndex = 0; userIndex < room.sideTwo.length; userIndex++) {
+                    if (room.sideTwo[userIndex] + "" !== userObject._id + "") {
+                        workingArray.push(room.sideTwo[userIndex])
+                    }
+                }
+                room.sideTwo = workingArray
+            }
+            else {
+                for (let userIndex = 0; userIndex < room.sideOne.length; userIndex++) {
+                    if (room.sideOne[userIndex] + "" !== userObject._id + "") {
+                        workingArray.push(room.sideOne[userIndex])
+                    }
+                }
+                room.sideOne = workingArray
+            }
+
+            // temporarily save the room -- the original user will have left even if we have an error later
+            await room.save()
+            // Now the interesting part -- find a person that we'll slot onto the end of the workingArray
+            let newUser = await UserModel.findOne(roomSelectionCriteria(room.spawningUser, targetLookingFor, targetIdentity))
+                .select("_id")
+                .exec()
+
+            if (!newUser) {
+                return reject("Could not find a suitable user to replace original in room swap")
+            }
+            workingArray.push(newUser)
+
+            // now re-save
+            await room.save()
+            resolve("Successfully swapped out the user")
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
+}
+
+module.exports = {removeUserFromRoom, replaceUserInRoom}
