22566ece2073472c6d9b2be73e23fa9e90faf37b
Elliot Potter <elliotpotter496@gmail.com>
2023-08-22 23:32:44 -0400

    separate create functions

---
+++ routes/user/create-utility.js
@@ -0,0 +1,73 @@
+const {joinProperGroups} = require("../userGroup/joinProperGroups");
+const UserModel = require("../../models/UserModel");
+const PRIORITY_TIME_INC_MS = 1000 * 60 * 60 * 24 * 30     // 1 month
+
+async function createUtility({identity, age, lookingFor, cognitoId, phoneNumber, firstName,
+                                 unixBirthDate, location, ageRange, referringUser}) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const groups = await joinProperGroups({
+                identity, age, lookingFor, location, userScore: 0, dateChange: 0
+            })
+
+            console.log("CREATE-USER: Generating the user object")
+            let user = new UserModel({
+                // explicit fields
+                cognitoId: cognitoId,
+                phoneNumber: phoneNumber,
+                firstName: firstName,
+                identity: identity,
+                dateOfBirth: unixBirthDate,
+                location,
+
+                // emailAddress: req.body.emailAddress,
+
+                age: age,
+                ageRange,
+
+                lookingFor: lookingFor.filter(lookingForElem => !!lookingForElem),
+                // shortTerm: !!req.body.shortTerm,
+                userGroups: groups,
+            })
+
+            // mark the referringUser if they exist
+            if (referringUser) {
+                user.referringUser = referringUser
+            }
+
+            console.log("CREATE-USER: Saving the user object")
+            let savedUser = await user.save()
+
+            // if referringUser exists, get them and increase their priority time
+            if (referringUser) {
+                const referringUserObject = await UserModel.findOne({_id})
+                    .select(['priorityMode', 'priorityModeExpiryTime'])
+                    .exec()
+
+                if (referringUserObject) {
+                    referringUserObject.priorityMode = true
+                    // if an expiry time exists and it's in the future
+                    if (referringUserObject.priorityModeExpiryTime &&
+                        (new Date(referringUserObject.priorityModeExpiryTime).getTime()) > Date.now()) {
+                        // expiry time is the old time plus 1 month
+                        referringUserObject.priorityModeExpiryTime =
+                            (new Date(referringUserObject.priorityModeExpiryTime).getTime()) + PRIORITY_TIME_INC_MS
+                    }
+                    else {
+                        // otherwise, set it to now plus a month
+                        referringUserObject.priorityModeExpiryTime = Date.now() + PRIORITY_TIME_INC_MS
+                    }
+
+                    await referringUserObject.save()
+                }
+            }
+
+            return resolve(savedUser)
+        }
+        catch(err) {
+            reject(err)
+        }
+    })
+}
+
+module.exports = {createUtility}
--- routes/user/create.js
+++ routes/user/create.js
@@ -2,8 +2,7 @@ const UserModel = require("../../models/UserModel");
 const {findClosestGroup} = require("../userGroup/findClosestGroup");
 const {generateGroup} = require("../userGroup/generateGroup");
 const {joinProperGroups} = require("../userGroup/joinProperGroups");
-
-const PRIORITY_TIME_INC_MS = 1000 * 60 * 60 * 24 * 30     // 1 month
+const {createUtility} = require("./create-utility");
 
 module.exports = (router) => {
     router.post('/create-user', async (req, res) => {
@@ -34,61 +33,10 @@ module.exports = (router) => {
         }
 
         try {
-            const groups = await joinProperGroups({
-                identity, age, lookingFor, location, userScore: 0, dateChange: 0
+            // use the creation utility rather than doing this ourselves
+            const savedUser = await createUtility({
+                identity, age, lookingFor, cognitoId, phoneNumber, firstName, unixBirthDate, location, ageRange, referringUser
             })
-
-            console.log("CREATE-USER: Generating the user object")
-            let user = new UserModel({
-                // explicit fields
-                cognitoId: req.body.cognitoId,
-                phoneNumber: req.body.phoneNumber,
-                firstName: req.body.firstName,
-                identity: req.body.identity,
-                dateOfBirth: unixBirthDate,
-                location,
-
-                // emailAddress: req.body.emailAddress,
-
-                age: age,
-                ageRange,
-
-                lookingFor: lookingFor.filter(lookingForElem => !!lookingForElem),
-                // shortTerm: !!req.body.shortTerm,
-                userGroups: groups,
-            })
-
-            // mark the referringUser if they exist
-            if (referringUser) {
-                user.referringUser = referringUser
-            }
-
-            console.log("CREATE-USER: Saving the user object")
-            let savedUser = await user.save()
-
-            // if referringUser exists, get them and increase their priority time
-            if (referringUser) {
-                const referringUserObject = await UserModel.findOne({_id})
-                    .select(['priorityMode', 'priorityModeExpiryTime'])
-                    .exec()
-
-                if (referringUserObject) {
-                    referringUserObject.priorityMode = true
-                    // if an expiry time exists and it's in the future
-                    if (referringUserObject.priorityModeExpiryTime &&
-                        (new Date(referringUserObject.priorityModeExpiryTime).getTime()) > Date.now()) {
-                        // expiry time is the old time plus 1 month
-                        referringUserObject.priorityModeExpiryTime =
-                            (new Date(referringUserObject.priorityModeExpiryTime).getTime()) + PRIORITY_TIME_INC_MS
-                    }
-                    else {
-                        // otherwise, set it to now plus a month
-                        referringUserObject.priorityModeExpiryTime = Date.now() + PRIORITY_TIME_INC_MS
-                    }
-
-                    await referringUserObject.save()
-                }
-            }
 
             return res.status(200).json(savedUser._id)
         }
