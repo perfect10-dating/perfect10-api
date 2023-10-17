a09819c36af365150e5f3115480f53bc955dcfc7
Elliot Potter <elliotpotter496@gmail.com>
2023-10-17 19:53:55 -0400

    fix bug where mutual matches sometimes showed yourself

--- routes/lookup/showCrushList.js
+++ routes/lookup/showCrushList.js
@@ -3,6 +3,39 @@ const LookupRequestModel = require("../../models/LookupRequestModel");
 const {generateLookupQueries} = require("./generateLookupQueries");
 const ConversationModel = require("../../models/ConversationModel");
 
+/**
+ * given a list of mutual lookup requests, get the list of other userIds or other emails
+ * with which to find the people crushing on you.
+ *
+ * @param mutualCrushes
+ * @param ownUserId
+ * @param ownUserEmail
+ * @returns {{emails: *[], userIds: *[]}}
+ */
+const getUserIdsAndEmails = ({mutualCrushes, ownUserId}) => {
+  let userIds = []
+  let emails = []
+
+  for (let crush of mutualCrushes) {
+    // you initiated the crush
+    if (crush.lookingUser+"" === ownUserId+"") {
+      if (crush.queryUser) {
+        userIds.push(crush.queryUser)
+      }
+      if (crush.queryEmail) {
+        userIds.push(crush.queryEmail)
+      }
+    }
+
+    // the other person initiated the crush
+    else {
+      userIds.push(crush.lookingUser)
+    }
+  }
+
+  return {userIds, emails}
+}
+
 module.exports = (router) => {
   router.get('/show-crush-list', async (req, res) => {
     try {
@@ -45,11 +78,13 @@ module.exports = (router) => {
         ]
       }).lean().exec()
       // console.log(mutualLookupRequests)
-      let userIds = mutualLookupRequests.filter(req => !!req.queryUser).map(req => req.queryUser)
-      let emails = mutualLookupRequests.filter(req => !!req.queryEmail).map(req => req.queryEmail)
-      // console.log(userIds)
-      // console.log(emails)
-      
+
+      // get the userIds and emails for the other person in the mutual crush
+      const {userIds, emails} = getUserIdsAndEmails({
+        mutualCrushes: mutualLookupRequests,
+        ownUserId: ownUser._id
+      })
+
       let userModels = await UserModel.find({_id: userIds}).select(
         ["_id", "firstName", "identity", "age", "location", "photoLinks", "shortTerm"]
       ).lean().exec()
